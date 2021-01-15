4ee37faeafa357be42b47845aa787abc69f9d985
mengzhao <aa@qq.com>
2021-01-15 16:08:05 +0800

    branchtest

--- src/main/scala/Test2.scala
+++ src/main/scala/Test2.scala
@@ -1,6 +1,10 @@
+import org.apache.spark.sql.SparkSession
+
 object Test2 {
   def main(args: Array[String]): Unit = {
 
+
+    val ss = SparkSession.builder()
 
     println("zhaomeng")
     println("zhaomeng")
