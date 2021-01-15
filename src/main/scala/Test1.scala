ba4530450d00d528ee35a0d81806b6b765335d1f
username <email>
2021-01-15 22:08:10 +0800

    aaaa

---
+++ src/main/scala/Test1.scala
@@ -0,0 +1,32 @@
+package com.zm.test
+
+import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
+
+import scala.collection.mutable.ArrayBuffer
+
+object Test1 {
+  def main(args: Array[String]): Unit = {
+
+    var tes = ArrayBuffer[Int]()
+    //    tes += null
+    //    println(tes.seq)
+
+
+    val schema = StructType(List(
+      StructField("a", IntegerType),
+      StructField("b", IntegerType),
+      StructField("c", IntegerType)
+    ))
+    var setarray = ArrayBuffer[Set[Long]](Set(0))
+    for (set <- setarray) {
+      println("==")
+      if (set.contains(9)) {
+      } else {
+        setarray += Set(9)
+      }
+    }
+    println(setarray.size)
+  }
+
+
+}
